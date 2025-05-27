3c18604ebea5e130ed456f36cb2629a5186b178b
Virendra321y <virendrayadavvns123@gmail.com>
2025-05-27 23:28:43 +0530

    this is my first commit

---
+++ src/main/java/com/anshu/anshuProject/AnshuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.anshu.anshuProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnshuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnshuProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshu/anshuProject/AnshuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshu.anshuProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnshuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
